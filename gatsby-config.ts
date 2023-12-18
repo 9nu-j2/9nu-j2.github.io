c733d1984dedaae78d0133f8e8f6e9023f96fe0f
2-jun9 <jungu269@gmail.com>
2023-12-18 17:52:42 +0900

    setting blog: add metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,9 +2,10 @@ import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: ``,
+    title: `아직미정`,
     description: `문제를 해결하는게 재밌는 사람입니다. 이를 기록으로 남깁니다.`,
-    siteUrl: `https://www.yourdomain.tld`,
+    author: `JUN9`,
+    siteUrl: `https://9nu-j2.github.io`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
