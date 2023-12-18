c3563dd1f5c9e55fbaa6a009841cb31eb8a7e2b5
2-jun9 <jungu269@gmail.com>
2023-12-18 15:58:54 +0900

    setting blog: gatsby

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,25 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `blog`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: ``,
+    description: `문제를 해결하는게 재밌는 사람입니다. 이를 기록으로 남깁니다.`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-google-gtag"]
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-typescript',
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
+    'gatsby-plugin-styled-components',
+  ],
 };
 
 export default config;
