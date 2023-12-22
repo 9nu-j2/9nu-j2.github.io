f81c9641a4bb613f604c55583abe4b237c1b7766
2-jun9 <jungu269@gmail.com>
2023-12-22 18:00:57 +0900

    feature blog: post page

--- src/Components/Templates/Post.Header.tsx
+++ src/Components/Templates/Post.Header.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent } from 'react'
-import { styled } from 'styled-components';
+import { styled, keyframes } from 'styled-components';
 import Nav from '../Organisms/Navbar'
+
+import backg from '../../Statics/2.jpg'
 
 type HProp = {
     title: string
@@ -9,10 +11,27 @@ type HProp = {
 const PostHeader: FunctionComponent<HProp> = function ({ title }) {
     return (
         <Header>
+            <ImageStyle>
+            </ImageStyle>
             <Nav title={title}></Nav>
         </Header>
     )
 }
+
+const move = keyframes`
+    0%{
+        background-color: rgba( 255, 255, 255, 0.1 );
+    }
+    35%{
+        background-color: rgba( 255, 255, 255, 0.2 );
+    }
+    70%{
+        background-color: rgba( 255, 255, 255, 0.3 );
+    }
+    100%{
+        background-color: rgba( 255, 255, 255, 0.5 );
+    }
+`
 
 const Header = styled.div`
     width: 100%;
@@ -22,6 +41,18 @@ const Header = styled.div`
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
+`
+
+const ImageStyle = styled.div`
+    position: absolute;
+    top:0;
+    left:0;
+    width:100%;
+    height:760px;
+    z-index:-1;
+    background:url(${backg});
+    background-size: 100%;
+    animation: ${move} 2s linear infinite;
 `
 
 export default PostHeader
