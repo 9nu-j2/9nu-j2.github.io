59cc4140ab36f4819253a8b3d41d61ba759508e8
2-jun9 <jungu269@gmail.com>
2023-12-28 16:59:38 +0900

    feature blog: add mdxs

---
+++ src/Components/Templates/PostTemplate.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+interface PostTemplateProps {
+    children: JSX.Element;
+}
+
+const Container = styled.section``;
+
+function PostTemplate({ children }: PostTemplateProps) {
+    return <Container>{children}</Container>;
+}
+
+export default PostTemplate;
