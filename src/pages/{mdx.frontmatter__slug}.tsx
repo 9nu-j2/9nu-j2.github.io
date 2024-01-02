7bbac6e44cd32c5a697c692ccf64703a8ba9744e
2-jun9 <jungu269@gmail.com>
2024-01-02 17:08:45 +0900

    chore: 필요 없는 부분 삭제

--- src/pages/{mdx.frontmatter__slug}.tsx
+++ src/pages/{mdx.frontmatter__slug}.tsx
@@ -5,21 +5,6 @@ import PostBody from '../Components/Templates/Post.Body'
 import { DataProps } from 'Types/Types'
 import Footer from '../Components/Organisms/Footer'
 
-// type BlogProps = {
-//     data: {
-//         mdx: {
-//             frontmatter: {
-//                 title: string,
-//                 datePublished: string,
-//                 author: string,
-//                 slug: string,
-//             }
-//             id: string;
-//         }
-//     },
-//     children: any;
-// }
-
 const BlogPost = ({ data, children }) => {
     return (
         <div>
