bced3263dccf5fe0fb606fcfcc265b034dbbc80f
2-jun9 <jungu269@gmail.com>
2024-01-03 16:50:19 +0900

    feature: main page에 3D 리소스 반영

--- src/Components/Templates/Home.Header.tsx
+++ src/Components/Templates/Home.Header.tsx
@@ -1,21 +1,31 @@
-import React, { Suspense, FunctionComponent } from 'react'
+import React, { FunctionComponent, Suspense, useEffect, useRef, useState } from 'react'
 import { styled, keyframes } from 'styled-components';
 import Nav from '../Organisms/Navbar';
-import Introduction from '../Organisms/About.Me';
-import { MetaProps } from 'Types/Types';
-import { Canvas } from "@react-three/fiber";
 
+import { MetaProps } from 'Types/Types';
+import { Canvas } from '@react-three/fiber'
+import { Model } from "../Organisms/3dpea"
+import { Environment, ContactShadows, OrbitControls } from "@react-three/drei"
 
 const HeaderTemplate: FunctionComponent<MetaProps> = function ({ title, description, author }) {
     return (
         <Header>
             <Container>
-                <Canvas dpr={[1.5, 2]} linear shadows>
+                <Canvas camera={{ position: [-5, 0, -15], fov: 55 }}>
+                    <pointLight position={[10, 10, 10]} intensity={1.5} />
+                    <Suspense fallback={null}>
+                        <group rotation={[0, Math.PI, 0]} position={[0, 1, 0]}>
+                            <Model description={description} author={author} />
+                        </group>
+                        <Environment preset="city" />
+                    </Suspense>
+                    <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} />
+                    <OrbitControls enablePan={false} enableZoom={false} minPolarAngle={Math.PI / 2.2} maxPolarAngle={Math.PI / 2.2} />
                 </Canvas>
             </Container>
             <Nav title={title}></Nav>
             <TitlePart>
-                Jun9 <span style={{ fontSize: '0.4em' }}></span>
+                Test <span style={{ fontSize: '0.4em' }}></span>
                 <br />
                 <span>Blog</span>
             </TitlePart>
@@ -36,12 +46,12 @@ const Header = styled.div`
 
 const Container = styled.div`
     position: absolute;
-    /* top: 50%;
-    left: 50%; */
+    /* top: 50%; */
+    /* left: 50%; */
     transform: translate(-50%, -50%, 0%);
     width:100%;
     height: 100vh;
-    z-index:-1;
+    z-index:0;
 `
 
 const TitlePart = styled.div`
